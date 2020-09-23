067e3d57a90237e776b43ed5d5fe184e9ada1013
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-09-23 10:39:25 -0700

    production fixed

---
+++ templates/precache-manifest.b8f25226f7bbf133daee1a1da09b6859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386ffefa26489bac230237d8aa1bd77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cda946c9dddc52b4bd0",
+    "url": "/static/css/main.befd5a3f.chunk.css"
+  },
+  {
+    "revision": "be6cbba54827e88ba47b",
+    "url": "/static/js/2.3cf6479f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3cf6479f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cda946c9dddc52b4bd0",
+    "url": "/static/js/main.8fd8d5be.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
